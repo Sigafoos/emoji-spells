e16156f19f1ddf30fe1b327040990cb16b4b8ad6
Dan Conley <dconley@protonmail.com>
2017-04-12 11:17:19 -0400

    Switching to a separate interface

--- spell.go
+++ spell.go
@@ -11,25 +11,23 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
-	"time"
 
+	"github.com/Sigafoos/jsonrand"
 	"github.com/kyokomi/emoji"
 )
 
-var emoji_array []string
-var spell_type []string
-var spell_preposition []string
-var spell_subject []string
+var emoji_array jsonrand.String
+var spell_type jsonrand.String
+var spell_preposition jsonrand.String
+var spell_subject jsonrand.String
 var options map[string]string
 
 func main() {
-	emoji_array = LoadJson("emoji.json")
-	spell_type = LoadJson("type.json")
-	spell_preposition = LoadJson("preposition.json")
-	spell_subject = LoadJson("subject.json")
+	emoji_array.Load("emoji.json")
+	spell_type.Load("type.json")
+	spell_preposition.Load("preposition.json")
+	spell_subject.Load("subject.json")
 	options = LoadOptions("options.json")
-
-	rand.Seed(time.Now().UTC().UnixNano())
 
 	spell := GenerateSpell()
 	Post(spell)
@@ -39,16 +37,16 @@ func GenerateSpell() string {
 	var spell bytes.Buffer
 
 	spell.WriteString("[")
-	spell.WriteString(RandomArg(spell_type))
+	spell.WriteString(spell_type.Element())
 	spell.WriteString(" ")
-	spell.WriteString(RandomArg(spell_preposition))
+	spell.WriteString(spell_preposition.Element())
 	spell.WriteString(" ")
-	spell.WriteString(RandomArg(spell_subject))
+	spell.WriteString(spell_subject.Element())
 
 	spell.WriteString("]\n\n")
 
 	for i := 0; i < rand.Intn(3)+3; i++ {
-		spell.WriteString(emoji.Sprint(RandomArg(emoji_array)))
+		spell.WriteString(emoji.Sprint(emoji_array.Element()))
 	}
 	return strings.ToUpper(spell.String())
 }
@@ -65,21 +63,6 @@ func EmojiList(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, GenerateSpell())
 }
 
-func LoadJson(filename string) []string {
-	var parsed []string
-
-	file, e := ioutil.ReadFile(filename)
-	if e != nil {
-		fmt.Printf("File Error: [%v]\n", e)
-	}
-	e = json.Unmarshal(file, &parsed)
-	if e != nil {
-		fmt.Printf("File Error: [%v]\n", e)
-	}
-
-	return parsed
-}
-
 func LoadOptions(filename string) map[string]string {
 	var parsed map[string]string
 	file, e := ioutil.ReadFile(filename)
@@ -92,10 +75,6 @@ func LoadOptions(filename string) map[string]string {
 	}
 
 	return parsed
-}
-
-func RandomArg(subject []string) string {
-	return subject[rand.Intn(len(subject)-1)]
 }
 
 func Post(spell string) {
@@ -117,5 +96,7 @@ func Post(spell string) {
 	if err != nil {
 		panic(fmt.Sprintf("ERROR with req: %v\n", err))
 	}
-	resp.Body.Close()
+	defer resp.Body.Close()
+	fmt.Println(spell)
+	//io.Copy(os.Stdout, resp.Body)
 }
